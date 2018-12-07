bd6e79fd9f41481a53beb60a0ebac4e9e72fa137
ebalczewski <balczewskie@carleton.edu>
2018-12-07 17:03:34 -0500

    First draft: Models and Node

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var user = require('./models.js');
+
+const server = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  let response = '';
+  try {
+    let users = await user.findAll()
+    var allNames = '';
+    for (let result of users) {
+       allNames = allNames + result.lastName;
+    }
+    response = allNames;
+  }
+  catch(err) {
+    response = err.stack;
+  }
+  res.end(response);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ backend/models.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+
+// Or you can simply use a connection uri
+const sequelize = new Sequelize('sqlite:rcmapp.db');
+
+const User = sequelize.define('user', {
+  email: {
+     type: Sequelize.STRING,
+     unique: true,
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowsNull: true,
+    defaultValue: null,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowsNull: true,
+    defaultValue: null,
+  },
+  batch: {
+    type: Sequelize.STRING,
+    allowsNull: true,
+    defaultValue: null
+  },
+});
+
+module.exports = User;
+
+// force: true will drop the table if it already exists
+// User.sync();
+
+//.then(() => {
+  // Table created
+  //return User.create({
+    // firstName: 'John',
+    //lastName: 'Hancock',
+    //email: 'john@hanckock.com',
+    //batch: 'W1865',
+//  });
+//});
