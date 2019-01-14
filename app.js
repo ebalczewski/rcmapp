a404348e8d1908cfbb57102e6b726f0b59a00060
ebalczewski <balczewskie@carleton.edu>
2019-01-14 17:15:50 -0500

    Created a multi-table getter for coordinates by userId

--- app.js
+++ app.js
@@ -21,6 +21,29 @@ app.get('/users', function (req, res) {
 	});
 });
 
+function getById(model, id) {
+	return model.find({
+		where: {
+			id: id
+		}
+	})
+}
+
+app.get('/users/:userId/addresses/coordinates/', function (req, res) {
+	let userId = req.params.userId;
+
+	getById(User, userId).then(function(user) {
+		user.getAddresses().then(function(addresses) {
+			res.json({latitude: addresses[0].latitude, 
+					longitude: addresses[0].longitude});
+		})
+	});
+});
+
+
+//latitudes and longitudes within a range
+//
+
 //app.use(express.static('static'));
 
 app.listen(port, () => console.log(`Serving on port ${port}.`))
