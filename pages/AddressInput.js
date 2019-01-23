f254dc26b713a1e96182d3f46980405968f562c8
ebalczewski <balczewskie@carleton.edu>
2019-01-23 16:19:36 -0500

    Highlights first suggestion.

--- pages/AddressInput.js
+++ pages/AddressInput.js
@@ -11,23 +11,26 @@ const API_KEY = process.env.GOOGLE_MAPS_JAVASCRIPT_API_KEY
 class AddressInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: '' };
+    this.state = { 
+      address: '',
+      lat: null,
+      lng: null};
   }
 
-  handleChange = address => {
+  handleChange = (address) => {
     this.setState({ address });
   };
 
   handleSubmit = (event) => {
     (event).preventDefault();
-    console.log("submit")
-    // geocodeByAddress(address)
-    // .then(results => getLatLng(results[0]))
-    // .then(latLng => console.log('Success', latLng))
-    // .catch(error => console.error('Error', error));
+    
+    geocodeByAddress(address)
+    .then(results => getLatLng(results[0]))
+    .then(latLng => console.log('Success', latLng))
+    .catch(error => console.error('Error', error));
   }
 
-  handleSelect = address => {
+  handleSelect = (address) => {
     this.setState({ address })
   };
 
@@ -41,6 +44,7 @@ class AddressInput extends React.Component {
           value={this.state.address}
           onChange={this.handleChange}
           onSelect={this.handleSelect}
+          highlightFirstSuggestion={true}
         >
           {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
             <div>
