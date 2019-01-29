a0b3a459f4b01c144d8ed5f6fba0923c1fc815b3
Patrick Schneeweis <pschneeweis@protonmail.com>
2019-01-29 14:37:03 -0500

    Cleanup after merge.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-import FormContainer from './FormContainer';
-import UserInfoDisplay from './UserInfoDisplay'
-import MapContainer from './MapContainer';
-import Login from './Login';
-
 import Banner from '../components/stateless/Banner';
 import MapContainer from '../components/MapContainer';
 import AddressInput from '../components/AddressInput';
@@ -22,13 +17,7 @@ export default class extends React.Component {
   constructor(props) {
       super(props)
 
-      this.onLoginSucess = this.onLoginSucess.bind(this);
-
       this.state = {
-          isLoggedIn: false,
-          userEmail: null,
-          showUserInfo: true,
-          editUserInfo: true,
           userEmail: props.userEmail,
           firstName: props.firstName,
           lastName: props.lastName,
@@ -37,62 +26,6 @@ export default class extends React.Component {
   }
 
   componentDidMount() {
-    //
-  }
-
-  onLoginSucess(email) {
-    this.setState({
-      isLoggedIn: true,
-      userEmail: email
-    })
-  }
-
-  renderBanner = () => {
-    return(
-      <p>Banner here!</p>
-    );
-  }
-
-  renderAuthentication = () => {
-    if (!this.state.isLoggedIn) {
-      return(
-        <p>Please authenticate yourself!</p>
-      )
-    }
-  }
-  
-  renderUserInfo = () => {
-    if (this.state.showUserInfo) {
-      if (this.state.editUserInfo) {
-        return(
-          <div>
-            <FormContainer />
-          </div>
-        );
-      } else if (!this.state.editUserInfo) {
-        return(
-          <div>
-            <UserInfoDisplay />
-          </div>
-        )
-      }
-    }
-    return(null);
-  }
-  //Probably will add 1 more component with filters/searching/preferences
-  renderMap = () => {
-    if (this.state.isLoggedIn) {
-      return(
-        <div>
-          <MapContainer />
-        </div>
-      );
-    } else {
-      return(
-        <div>
-          <Login action={this.onLoginSucess} />
-        </div>);
-    }
 
   }
   //Also probs want a footer
